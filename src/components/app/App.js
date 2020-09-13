f6177a749ebae98060083c422f8fa5469db4a5e4
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-09-13 14:53:36 +0430

    init projects closes #1 add book list container to app closes #2

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import BookList from "../../containers/book-list";
+
+function App() {
+  return (
+    <div className="App">
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+export default function () {
+  return [
+    {
+      title: "Javascript: The good practice",
+    },
+    {
+      title: "Harry potter",
+    },
+    {
+      title: "The dark tower",
+    },
+    {
+      title: "Eloquent ruby",
+    },
+  ];
+}
